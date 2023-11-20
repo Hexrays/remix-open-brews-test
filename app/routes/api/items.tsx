3474f874dfeda94e39bfce503f959fbe9159a51a
Jeff Denton <jeff@clariumhealth.com>
2023-11-20 12:56:08 -0500

    make routes public

--- app/routes/api/items.tsx
+++ app/routes/api/items.tsx
@@ -2,5 +2,13 @@ import { json } from "remix";
 import { fakeItems } from "~/models/test";
 
 export async function loader() {
-  return json({ data: fakeItems });
+  return json(
+    { data: fakeItems },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE",
+      },
+    }
+  );
 }
--- app/routes/api/locations.tsx
+++ app/routes/api/locations.tsx
@@ -2,5 +2,13 @@ import { json } from "remix";
 import { fakeLocations } from "~/models/test";
 
 export async function loader() {
-  return json({ data: fakeLocations });
+  return json(
+    { data: fakeLocations },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE",
+      },
+    }
+  );
 }
